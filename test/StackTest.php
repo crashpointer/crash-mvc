d71b1f2bd1a2aec4cb9fba35fb4c5d21cffeba5e
Ceren TOKAY <smyrna.trk@gmail.com>
2017-12-14 18:05:33 +0300

    add: created unit test

---
+++ test/StackTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of StackTest
+ *
+ * @author Ceren
+ */
+class StackTest extends TestCase{
+    
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+    
+}
